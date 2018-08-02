49eaa9eb85673574fe78145aaebd67f9991ea028
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 23:13:46 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
-  validates :name, presence => true 
-  validates_uniqueness_of :email 
+  validates :name, presence => true
+  validates_uniqueness_of :email
 end
