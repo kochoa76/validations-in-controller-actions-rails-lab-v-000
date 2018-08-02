0e728fa243718d9f6a618b347be318266fcd3800
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 23:21:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
-  validates :category, inclusion: {in: w%(Fiction Non-fiction)}
+  validates :category, inclusion:{ in: %w(Fiction Non-Fiction) }
   validates :content, length: {minimum: 100}
 
 end
